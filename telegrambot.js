c1384e9d62fe842814640f99ece1870725036366
Mark Brooks <Markabrooks@gmail.com>
2020-12-28 22:16:52 -0700

    add telegram bot functionality so we can send messages later and added some info to the README

---
+++ telegrambot.js
@@ -0,0 +1,39 @@
+//this file doesn't really do anything because I don't know how to run it :)
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+
+
+ 
+const token = process.env.TELEGRAM_TOKEN;
+let bot;
+
+// if (process.env.NODE_ENV === 'production') {
+//    bot = new TelegramBot(token);
+//    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+// } else {
+   bot = new TelegramBot(token, { polling: true });
+// }
+
+bot.sendMessage(process.env.MARK_TELEGRAM_CHAT_ID, "Hi Mark Ski Scraper Bot has started!")
+bot.sendMessage(process.env.JOHN_TELEGRAM_CHAT_ID, "Hi John Ski Scraper Bot has started!")
+
+bot.on('message', (msg) => {
+
+    var hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Thanks for using Ski Scraper!  Here is your chat ID" + msg.chat.id);
+    } 
+        
+    var bye = "bye";
+    if (msg.text.toString().toLowerCase().includes(bye)) {
+    bot.sendMessage(msg.chat.id, "See you on the slopes, Bye");
+    } 
+    
+    });
+
+    bot.onText(/\/start/, (msg) => {
+
+        bot.sendMessage(msg.chat.id, "Welcome");
+            
+        });
+      
