d3bd7c85051ffa9f0db6de51b1a89b9b0a890ad5
Mark Brooks <Markabrooks@gmail.com>
2021-01-02 17:30:18 -0700

    add dates for mark

--- worker/user_data.js
+++ worker/user_data.js
@@ -100,33 +100,38 @@ const userData = {
         telegram_id: process.env.MARK_TELEGRAM_CHAT_ID,
         desiredSkiDays: {
             "winterpark": {
-                "2020-19-10": {
-                    hasBeenNotified: true
+                "2021-02-13": {
+                    hasBeenNotified: false
                 },
-            //     "2020-13-25": {
-            //         hasBeenNotified: false
-            //     },
-            //     "2021-01-09": {
-            //         hasBeenNotified: false
-            //     },
-            //     "2021-01-04": {
-            //         hasBeenNotified: false
-            //     }
-            // },
-            // "abasin": {
-            //     "2021-01-16": { // unavailable
-            //         hasBeenNotified: false
-            //     },
-            //     "2021-01-29": { // available
-            //         hasBeenNotified: false
-            //     },
-            //     "2021-02-16": {
-            //         hasBeenNotified: true
-            //     },
-            //     "2021-04-08": {
-            //         hasBeenNotified: false
-            //     }
             },
+            "abasin": {
+                "2021-01-23": {
+                    hasBeenNotified: false
+                },
+                "2021-01-24": {
+                    hasBeenNotified: false
+                },
+                "2021-01-10": {
+                    hasBeenNotified: false
+                },
+                "2021-01-09": {
+                    hasBeenNotified: false
+                },
+                "2021-01-16": {
+                    hasBeenNotified: false
+                },
+                "2021-01-17": {
+                    hasBeenNotified: false
+                },
+            },
+            "taos": {
+                "2021-03-06": {
+                    hasBeenNotified: false
+                },
+                "2021-03-07": {
+                    hasBeenNotified: false
+                },
+            }
         }
     }
 }
