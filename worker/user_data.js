e15b23b84d3058a232a1f5140d1154ceaf4cb3a7
Mark Brooks <Markabrooks@gmail.com>
2021-01-10 14:42:50 -0700

    change up user_data for our new redis schema

--- worker/user_data.js
+++ worker/user_data.js
@@ -3,138 +3,77 @@
 
 const userData = {
 
-    // "john": {
-    //     passType: "base",
-    //     //telegram_id: process.env.JOHN_TELEGRAM_CHAT_ID,
-    //     telegram_id: process.env.TELEGRAM_TEST_CHAT_ID,
-    //     desiredSkiDays: {
-    //         "winterpark": {
-    //             "2021-01-23": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-01-24": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-01-10": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-01-09": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-01-16": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-01-17": {
-    //                 hasBeenNotified: false
-    //             },
-    //         },
-    //         "abasin": {
-    //             // "2021-01-27":{ // this is a test
-    //             //     hasBeenNotified: false
-    //             // },
-    //             "2021-02-06": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-02-13":{ //blackout date
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-02-07": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-02-14": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-02-20": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-02-21": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-02-27": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-02-28": {
-    //                 hasBeenNotified: false
-    //             }
-    //         },
-    //         "taos": {
-    //             "2021-03-13": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-03-14": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-03-20": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-03-21": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-03-27": {
-    //                 hasBeenNotified: false
-    //             },
-    //             "2021-03-28": {
-    //                 hasBeenNotified: false
-    //             },
-    //         }
-    //     }
-    // },
-    // "louis": {
-    //     passType: "base",
-    //     telegram_id: process.env.LOUIS_TELEGRAM_CHAT_ID,
-    //     desiredSkiDays: {
-    //         "winterpark": {
-    //             "2021-02-13": {
-    //                 hasBeenNotified: false
-    //             },
-    //         },
-    //     }
-    // },
-    "mark": {
-        passType: "base",
-        //telegram_id: process.env.MARK_TELEGRAM_CHAT_ID,
-        telegram_id: process.env.TELEGRAM_TEST_CHAT_ID,
-        desiredSkiDays: {
-            "winterpark": {
-                "2021-02-13": {
-                    hasBeenNotified: false
-                },
-                //this guy should always be available used for testing
-                "2021-01-20": {
-                    hasBeenNotified: false
-                },
-            },
-            "abasin": {
-                "2021-01-23": {
-                    hasBeenNotified: false
-                },
-                "2021-01-24": {
-                    hasBeenNotified: false
-                },
-                "2021-01-10": {
-                    hasBeenNotified: false
-                },
-                "2021-01-09": {
-                    hasBeenNotified: false
-                },
-                "2021-01-16": {
-                    hasBeenNotified: false
-                },
-                "2021-01-17": {
-                    hasBeenNotified: false
-                },
-            },
-            "taos": {
-                "2021-03-06": {
-                    hasBeenNotified: false
-                },
-                "2021-03-07": {
-                    hasBeenNotified: false
-                },
-            }
-        }
-    }
+    ///////////////////////////////////
+    //           Test Chat           //
+    //////////////////////////////////
+    "782458934:passType": "base",
+    "782458934:winterpark":[
+        "2021-02-13",
+    ],
+    "782458934:abasin":[
+        "2021-01-23",
+        "2021-01-24",
+        "2021-01-10",
+        "2021-01-09",
+        "2021-01-16",
+        "2021-01-17",
+    ],
+    "782458934:taos":[
+        "2021-03-06",
+        "2021-03-07",
+        "2021-01-04",
+    ],
+    ///////////////////////////////////
+    //           John                //
+    //////////////////////////////////
+    "719487075:passType": "base",
+    "719487075:winterpark": [
+        "2021-01-23",
+        "2021-01-24",
+        "2021-01-10",
+        "2021-01-09",
+        "2021-01-16",
+        "2021-01-17",
+    ],  
+    "719487075:abasin": [
+        "2021-02-06",
+        "2021-02-13",
+        "2021-02-07",
+        "2021-02-14",
+        "2021-02-20",
+        "2021-02-21",
+        "2021-02-27",
+        "2021-02-28",
+    ],
+    "719487075:taos": [
+                "2021-03-13",
+                "2021-03-14",
+                "2021-03-20",
+                "2021-03-21",
+                "2021-03-27",
+                "2021-03-28",
+    ],
+    ///////////////////////////////////
+    //           Mark                //
+    //////////////////////////////////
+    "782458934:passType": "base",
+    "782458934:winterpark":[
+        "2021-02-13",
+    ],
+    "782458934:abasin":[
+        "2021-01-23",
+        "2021-01-24",
+        "2021-01-10",
+        "2021-01-09",
+        "2021-01-16",
+        "2021-01-17",
+    ],
+    "782458934:taos":[
+        "2021-03-06",
+        "2021-03-07",
+        "2021-01-04",
+    ]
+      
 }
-
 
 module.exports = { userData: userData };
