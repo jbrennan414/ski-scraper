eee81c5b0290072b80ddd222d779827f2d202120
John Brennan <jbrennan@fluentstream.com>
2021-02-07 15:24:40 -0700

    fix case sensitivity of add and Add, add a preceding zero when a user adds a single digit month or day

--- addDate.js
+++ addDate.js
@@ -9,6 +9,21 @@ async function addDate(date, resort, telegram_id) {
         return;
     }
 
+    let splitDate = date.split("-")
+    let year = splitDate[0];
+    let month = splitDate[1];
+    let day = splitDate[2];
+
+    if (month.length == 1){
+        month = `0${month}`;
+    }
+
+    if (day.length == 1){
+        day = `0${day}`;
+    }
+
+    date = `${year}-${month}-${day}`    
+
     const key = telegram_id + ":" + resort
     await setAsync(key,date);
 
